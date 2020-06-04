edfce54237e79c84356c7930db1696c1ca214b27
Justin Ang <justinang13@gmail.com>
2020-06-04 12:35:27 -0700

    Added:   heap.js-     function heapifyDown()     function print()     general test cases

--- heap.js
+++ heap.js
@@ -23,9 +23,14 @@
 // sift-down: move a node down in the tree, similar to sift-up; used to restore heap condition after deletion or replacement.
 
 class Heap {
-  constructor(init) {
+  constructor(init, arr) {
     this.heapArr = [];
     this.heapType = init.type;
+    if(arr) {
+      for(let i = 0; i < arr.length; i++) {
+        this.insert(arr[i])
+      }
+    }
   }
 
   insert(val) {
@@ -35,11 +40,11 @@ class Heap {
   }
 
   delete() {
-    console.log("Popping element off the heap...");
+    // console.log("Popping element off the heap...");
     let lastVal = this.heapArr.pop();
     let returnVal = this.heapArr[0];
     this.heapArr[0] = lastVal;
-    this.heapify();
+    this.heapfiyDown();
     return returnVal;
   }
 
@@ -79,18 +84,83 @@ class Heap {
           break;
       }
       parent -= 1;
-      console.log("CURRENT PARENT VAL" + parent)
     }
-    // console.log(this.heapArr)
+  }
+
+  heapfiyDown() {
+    let parent = 0
+    while (parent < this.heapArr.length) {
+
+      let idx1 = (parent * 2) + 1, idx2 = (parent * 2) + 2;
+
+      let childLeft = this.heapArr[idx1];
+      let childRight = this.heapArr[idx2];
+      let parentNode = this.heapArr[parent];
+
+      let largerChildIdx = (typeof childLeft === "undefined") ? idx2 : 
+        (typeof childRight === "undefined") ? idx1 : undefined;
+      if(!largerChildIdx && (childLeft && childRight)) {
+        largerChildIdx = (this.heapType === "max-heap") ? 
+        (childLeft > childRight ? idx1 : idx2) : (childLeft > childRight ? idx2 : idx1)
+      }
+
+      switch(this.heapType) {
+        case "max-heap":
+          if(largerChildIdx && this.heapArr[largerChildIdx] > parentNode) {
+            let tmp = this.heapArr[largerChildIdx];
+            this.heapArr[largerChildIdx] = this.heapArr[parent];
+            this.heapArr[parent] = tmp;
+          }
+          break;
+        case "min-heap":
+          if(largerChildIdx && this.heapArr[largerChildIdx] < parentNode) {
+            let tmp = this.heapArr[largerChildIdx];
+            this.heapArr[largerChildIdx] = this.heapArr[parent];
+            this.heapArr[parent] = tmp;
+          }
+          break;
+        }
+      parent++;
+    }
+  }
+  
+  print() {
+    let newLine = 1;
+    for(let i = 0; i < this.heapArr.length; i++) {
+      if((i+1) == newLine) {
+        newLine *= 2;
+        console.log();
+      }
+      process.stdout.write(this.heapArr[i] + " ");
+    }
+    console.log();
   }
 }
 
+// Initialize Max and Min Heaps
 testHeap = new Heap(init = { type: "max-heap"});
+testHeap2 = new Heap(init = { type: "min-heap"}, [-1, 10, 100, 1000, 99]);
 
 testHeap.insert(5);
 testHeap.insert(100);
 testHeap.insert(300);
 testHeap.insert(999999999999);
 testHeap.insert(-1);
+testHeap.insert(-2);
+testHeap.insert(-3);
+testHeap.insert(-4);
+testHeap.insert(-5);
+console.log("Current heap structure: ")
+testHeap.print();
 
-console.log(testHeap.delete());
+console.log("Highest Element: " + testHeap.delete());
+testHeap.print();
+console.log("Highest Element: " + testHeap.delete());
+testHeap.print();
+// testHeap.delete();
+// testHeap.print();
+// testHeap.delete();
+// testHeap.print();
+
+console.log("Current heap structure: ")
+testHeap2.print();
